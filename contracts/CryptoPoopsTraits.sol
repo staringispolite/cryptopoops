98a68ac18165c6b997fddc6ac8f15e98fb7d485f
Jonathan Howard <howard.jonathan@gmail.com>
2021-04-04 15:55:47 -0700

    Adds trait lookups. Multi-dim array issues resolved, compiler just doesn't support

--- contracts/CryptoPoopsTraits.sol
+++ contracts/CryptoPoopsTraits.sol
@@ -17,7 +17,7 @@ contract CryptoPoopTraits {
   uint8 internal constant NUM_LEVELS = 4;
 
   // Categories within which we'll randomly assign
-  uint8 internal constant BACKGROUND = 0;
+  uint8 internal constant BACKGROUNDS = 0;
   uint8 internal constant BACK_ACCESSORIES = 1;
   uint8 internal constant BODIES = 2;
   uint8 internal constant COLORS = 3;
@@ -39,70 +39,73 @@ contract CryptoPoopTraits {
   ];
 
   // List the option numbers that fall into each category in each rarity level
-  // @dev uint8[NUM_LEVELS][NUM_CATEGORIES] = [1, 3, 10, 12];
-  /*
-  uint8[NUM_LEVELS][] internal immutable backgroundOptions = [
-    [0,2,3,9],
-    [1,4,10],
-    [5,8],
-    [6,7]
-  ];
-  uint8[NUM_LEVELS] internal immutable backAccessoryOptions = [
-    [0,2,3,9],
-    [1,4,10],
-    [5,8],
-    [6,7]
-  ];
-  uint8[NUM_LEVELS] internal immutable bodyOptions = [
-    [0,2,3,9],
-    [1,4,10],
-    [5,8],
-    [6,7]
-  ];
-  uint8[NUM_LEVELS] internal immutable colorOptions = [
-    [0,2,3,9],
-    [1,4,10],
-    [5,8],
-    [6,7]
-  ];
-  uint8[NUM_LEVELS] internal immutable faceOptions = [
-    [0,2,3,9],
-    [1,4,10],
-    [5,8],
-    [6,7]
-  ];
-  uint8[NUM_LEVELS] internal immutable frontAccessoryOptions = [
-    [0,2,3,9],
-    [1,4,10],
-    [5,8],
-    [6,7]
-  ];
-  uint8[NUM_CATEGORIES][NUM_LEVELS][] internal immutable traitLookup = [
-    backgroundOptions,
-    backAccessoryOptions,
-    bodyOptions,
-    colorOptions,
-    faceOptions,
-    frontAccessoryOptions
-  ];
-  */
+  // @dev optionsArray[level] = [1, 3, 10, 12];
+  uint8[][NUM_LEVELS] internal backgroundOptions;
+  uint8[][NUM_LEVELS] internal backAccessoryOptions;
+  uint8[][NUM_LEVELS] internal bodyOptions;
+  uint8[][NUM_LEVELS] internal colorOptions;
+  uint8[][NUM_LEVELS] internal faceOptions;
+  uint8[][NUM_LEVELS] internal frontAccessoryOptions;
 
-  uint internal nonce = 0;
+  // traitLookup[category][level][i] = an optionId
+  uint8[][NUM_LEVELS][NUM_CATEGORIES] internal traitLookup;
+
+  uint internal traitNonce = 0;
+
+  mapping(address=>uint256) internal traitStore;
+
+  constructor() {
+    // Initialize lookup data
+    backgroundOptions[COMMON] = [uint8(0), 2, 3, 9];
+    backgroundOptions[UNCOMMON] = [uint8(1), 4, 10];
+    backgroundOptions[RARE] = [uint8(5), 8];
+    backgroundOptions[LEGENDARY] = [uint8(6), 7];
+    backAccessoryOptions[COMMON] = [uint8(0), 2, 3, 9];
+    backAccessoryOptions[UNCOMMON] = [uint8(1), 4, 10];
+    backAccessoryOptions[RARE] = [uint8(5), 8];
+    backAccessoryOptions[LEGENDARY] = [uint8(6), 7];
+    bodyOptions[COMMON] = [uint8(0), 2, 3, 9];
+    bodyOptions[UNCOMMON] = [uint8(1), 4, 10];
+    bodyOptions[RARE] = [uint8(5), 8];
+    bodyOptions[LEGENDARY] = [uint8(6), 7];
+    colorOptions[COMMON] = [uint8(0), 2, 3, 9];
+    colorOptions[UNCOMMON] = [uint8(1), 4, 10];
+    colorOptions[RARE] = [uint8(5), 8];
+    colorOptions[LEGENDARY] = [uint8(6), 7];
+    faceOptions[COMMON] = [uint8(0), 2, 3, 9];
+    faceOptions[UNCOMMON] = [uint8(1), 4, 10];
+    faceOptions[RARE] = [uint8(5), 8];
+    faceOptions[LEGENDARY] = [uint8(6), 7];
+    frontAccessoryOptions[COMMON] = [uint8(0), 2, 3, 9];
+    frontAccessoryOptions[UNCOMMON] = [uint8(1), 4, 10];
+    frontAccessoryOptions[RARE] = [uint8(5), 8];
+    frontAccessoryOptions[LEGENDARY] = [uint8(6), 7];
+
+    traitLookup = [
+      backgroundOptions,
+      backAccessoryOptions,
+      bodyOptions,
+      colorOptions,
+      faceOptions,
+      frontAccessoryOptions];
+  }
 
   function randomLevel() internal returns(uint8) {
-    uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce))) % NUM_LEVELS;
-    nonce++;
+    uint randomNumber = uint(keccak256(abi.encodePacked(
+      block.timestamp, msg.sender, block.number, traitNonce))) % NUM_LEVELS;
+    traitNonce++;
     return uint8(randomNumber);
   }
 
-  // traitLookup[_level][_category] contains an array of IDs for this level and category
+  // traitLookup[_category][_level] contains an array of IDs for this level and category
   // eg. we might have rare backgrounds defined as 1.png, 2.png, and 21.png
-  // in that case, traitLookup[RARE][BACKGROUNDS] yields [1, 2, 21];
+  // in that case, traitLookup[BACKGROUNDS][RARE] yields [1, 2, 21];
   function randomTrait(uint8 _level, uint8 _category) internal returns(uint8) {
-    uint8 numOptions = 1;  // traitLookup[_level][_category].length;
-    uint randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce))) % numOptions;
-    uint randomOptionId = 1;  // traitLookup[_level][_category][randomNumber];
-    nonce++;
+    uint256 numOptions =  traitLookup[_category][_level].length;
+    uint randomNumber = uint(keccak256(abi.encodePacked(
+      block.timestamp, msg.sender, block.number, traitNonce))) % numOptions;
+    uint randomOptionId = traitLookup[_category][_level][randomNumber];
+    traitNonce++;
     return uint8(randomOptionId);
   }
 
@@ -110,8 +113,8 @@ contract CryptoPoopTraits {
   // Currently, our structure is the following:
   // | unused byte | unused byte | front acc | faces | colors | bodies | back acc | backgrounds |
   //
-  // TODO: pack into a uint32 w/fewer options each.
-  function encodeDNA(uint8[NUM_CATEGORIES] memory _traits) internal returns(uint256) {
+  // TODO: pack into a uint32 w/fewer options each?
+  function encodeTraits(uint8[NUM_CATEGORIES] memory _traits) internal returns(uint256) {
     uint256 dna = 0;
     for (uint32 i; i < NUM_CATEGORIES; i++) {
       uint8 dnaPart = uint8(_traits[i]);
