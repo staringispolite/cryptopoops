c505c780b0b81eebb5f5cf1cfe670fbff472e2bd
Jonathan Howard <howard.jonathan@gmail.com>
2021-04-04 20:47:37 -0700

    finalize traits

--- contracts/CryptoPoopsTraits.sol
+++ contracts/CryptoPoopsTraits.sol
@@ -20,30 +20,14 @@ contract CryptoPoopTraits {
   uint8 internal constant BACKGROUNDS = 0;
   uint8 internal constant BACK_ACCESSORIES = 1;
   uint8 internal constant BODIES = 2;
-  uint8 internal constant COLORS = 3;
-  uint8 internal constant FACES = 4;
-  uint8 internal constant FRONT_ACCESSORIES = 5;
-  uint8 internal constant NUM_CATEGORIES = 6;
-
-  // ALL options within each category
-  // Number of background options is CATEGORY_OPTIONS[BACKGROUND]
-  // @dev hard-coding the number of categories because the compiler needs
-  // it to be in memory in order to use even an immutable variable.
-  uint8[6] internal CATEGORY_OPTIONS = [
-    uint8(10),  // Backgrounds
-    uint8(20),  // Back accessories
-    uint8(3),   // Bodies
-    uint8(5),   // Colors
-    uint8(30),  // Faces
-    uint8(20)   // Front accessories
-  ];
+  uint8 internal constant FACES = 3;
+  uint8 internal constant FRONT_ACCESSORIES = 4;
+  uint8 internal constant NUM_CATEGORIES = 5;
 
   // List the option numbers that fall into each category in each rarity level
-  // @dev optionsArray[level] = [1, 3, 10, 12];
   uint8[][NUM_LEVELS] internal backgroundOptions;
   uint8[][NUM_LEVELS] internal backAccessoryOptions;
   uint8[][NUM_LEVELS] internal bodyOptions;
-  uint8[][NUM_LEVELS] internal colorOptions;
   uint8[][NUM_LEVELS] internal faceOptions;
   uint8[][NUM_LEVELS] internal frontAccessoryOptions;
 
@@ -52,7 +36,10 @@ contract CryptoPoopTraits {
 
   uint internal traitNonce = 0;
 
-  mapping(address=>uint256) internal traitStore;
+  // Mapping from token ID to encoded trait
+  mapping (uint256 => uint64) internal _tokenTraits;
+
+  event TraitAssigned(address indexed tokenOwner, uint256 tokenId, uint64 encodedTraits);
 
   constructor() {
     // Initialize lookup data
@@ -68,10 +55,6 @@ contract CryptoPoopTraits {
     bodyOptions[UNCOMMON] = [uint8(1), 4, 10];
     bodyOptions[RARE] = [uint8(5), 8];
     bodyOptions[LEGENDARY] = [uint8(6), 7];
-    colorOptions[COMMON] = [uint8(0), 2, 3, 9];
-    colorOptions[UNCOMMON] = [uint8(1), 4, 10];
-    colorOptions[RARE] = [uint8(5), 8];
-    colorOptions[LEGENDARY] = [uint8(6), 7];
     faceOptions[COMMON] = [uint8(0), 2, 3, 9];
     faceOptions[UNCOMMON] = [uint8(1), 4, 10];
     faceOptions[RARE] = [uint8(5), 8];
@@ -85,9 +68,9 @@ contract CryptoPoopTraits {
       backgroundOptions,
       backAccessoryOptions,
       bodyOptions,
-      colorOptions,
       faceOptions,
-      frontAccessoryOptions];
+      frontAccessoryOptions
+    ];
   }
 
   function randomLevel() internal returns(uint8) {
@@ -101,7 +84,7 @@ contract CryptoPoopTraits {
   // eg. we might have rare backgrounds defined as 1.png, 2.png, and 21.png
   // in that case, traitLookup[BACKGROUNDS][RARE] yields [1, 2, 21];
   function randomTrait(uint8 _level, uint8 _category) internal returns(uint8) {
-    uint256 numOptions =  traitLookup[_category][_level].length;
+    uint numOptions =  traitLookup[_category][_level].length;
     uint randomNumber = uint(keccak256(abi.encodePacked(
       block.timestamp, msg.sender, block.number, traitNonce))) % numOptions;
     uint randomOptionId = traitLookup[_category][_level][randomNumber];
@@ -109,16 +92,16 @@ contract CryptoPoopTraits {
     return uint8(randomOptionId);
   }
 
-  // @dev a DNA of uint256 that can store up to 8 distinct categories of max 256 options each
+  // @dev an encoded uint64 that can store up to 8 distinct categories of max 32 options each
   // Currently, our structure is the following:
-  // | unused byte | unused byte | front acc | faces | colors | bodies | back acc | backgrounds |
   //
-  // TODO: pack into a uint32 w/fewer options each?
-  function encodeTraits(uint8[NUM_CATEGORIES] memory _traits) internal returns(uint256) {
-    uint256 dna = 0;
+  // | unused byte | unused byte | unused byte | front acc | faces | bodies | back acc | backgrounds |
+  function encodeTraits(uint8[NUM_CATEGORIES] memory _traits) internal pure returns(uint64) {
+    uint64 encodedTraits= 0;
     for (uint32 i; i < NUM_CATEGORIES; i++) {
-      uint8 dnaPart = uint8(_traits[i]);
-      dna |= (dnaPart << (8 * i));
+      uint64 chunk = uint64(_traits[i]);
+      encodedTraits |= (chunk << (8 * i));
     }
+    return encodedTraits;
   }
 }
