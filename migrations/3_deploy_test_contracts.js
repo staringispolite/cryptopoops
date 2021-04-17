ab87fac1d3d4e57605a516d8c344cad1888834c3
Jonathan Howard <howard.jonathan@gmail.com>
2021-04-16 17:37:52 -0700

    remove tests from migrations

--- migrations/3_deploy_test_contracts.js
+++ migrations/3_deploy_test_contracts.js
@@ -2,6 +2,7 @@ const TestableCryptoPoopTraits = artifacts.require("TestableCryptoPoopTraits");
 const TestableCryptoPoops = artifacts.require("TestableCryptoPoops");
 
 module.exports = async (deployer, network, addresses) => {
-  await deployer.deploy(TestableCryptoPoopTraits, {gas: 5000000});
-  await deployer.deploy(TestableCryptoPoops, {gas: 5000000});
+  // Commenting out for "real" deploys
+  // await deployer.deploy(TestableCryptoPoopTraits, {gas: 5000000});
+  // await deployer.deploy(TestableCryptoPoops, {gas: 5000000});
 }
