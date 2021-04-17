ea5554cf06bf6cdc83b0644a1b6a5f8780461e01
Jonathan Howard <howard.jonathan@gmail.com>
2021-04-16 17:44:30 -0700

    ensuring we can still sell out successfully

--- test/TestableCryptoPoops.js
+++ test/TestableCryptoPoops.js
@@ -17,7 +17,7 @@ contract("TestableCryptoPoops", async (accounts) => {
   let [owner, alice, bob] = accounts;
 
   // Paused by default, since the test takes 10mins to sell out supply
-  xit("should allow re-rolls at max supply", async () => {
+  xit("should allow buying all the way to max supply", async () => {
     const instance = await testableCryptoPoops.new("https://nftapi.com/cryptopoops/");
     await utils.setUpSale(instance, owner);
     await utils.advanceTimeAndBlock(300);
@@ -26,24 +26,16 @@ contract("TestableCryptoPoops", async (accounts) => {
     const buyResult = await instance.dropPoops(20,  // ***THESE 20 mint fine
       {from: bob, value: web3.utils.toWei("0.4", "ether")});
     const firstEncodedTraits = await instance.traitsOf(0);
-    console.log('dropped 20 poops');
     await utils.advanceTimeAndBlock(300);
 
     // Use up supply to unlock re-rolls
     for (let i = 0; i < 60; i++) {
-      await instance._test_mint100({
+      const bulkMintResult = await instance._test_mint100({
         from: alice, gas: "12000000" });
+      expect(bulkMintResult.receipt.status).to.equal(true);
       await utils.advanceTimeAndBlock(300);
-      console.log('minted 100 more');
     }
 
-    const reRollResult = await instance._test_reRollTraits(0, 0, {
-      from: bob, value: "80000000000000000"});
-    const secondEncodedTraits = await instance.traitsOf(0);
-
-    await expectEvent(reRollResult, "TraitAssigned", {
-      tokenOwner: bob, tokenId: new BN(0), encodedTraits: secondEncodedTraits });
-    expect(firstEncodedTraits.toString()).not.to.equal(secondEncodedTraits.toString);
   }).timeout(700000);
 
   it("should allow re-rolls before max supply if whitelisted", async () => {
@@ -54,7 +46,6 @@ contract("TestableCryptoPoops", async (accounts) => {
     const buyResult = await instance.dropPoops(20, 0,
       {from: bob, value: web3.utils.toWei("0.4", "ether")});
     const firstEncodedTraits = await instance.traitsOf(0);
-    console.log('dropped 20 poops');
     await utils.advanceTimeAndBlock(300);
   });
 
