8250f2983c0ad485c2f083933165c52455a0d2fa
Jonathan Howard <howard.jonathan@gmail.com>
2021-04-17 17:33:40 -0700

    ready to approve OpenSea proxies

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,14 @@ const CryptoPoopTraits = artifacts.require("CryptoPoopTraits");
 // true/false;
 
 module.exports = async (deployer, network, addresses) => {
+  // OpenSea proxy registry addresses for rinkeby and mainnet.
+  let proxyRegistryAddress = "";
+  if (network === 'rinkeby') {
+    proxyRegistryAddress = "0xf57b2c51ded3a29e6891aba85459d600256cf317";
+  } else {
+    proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
+  }
+
   await deployer.deploy(CryptoPoopTraits, {gas: 5000000});
   await deployer.deploy(CryptoPoops, "https://staringispolite.github.io/cp-api/json/", {gas: 5000000});
 };
